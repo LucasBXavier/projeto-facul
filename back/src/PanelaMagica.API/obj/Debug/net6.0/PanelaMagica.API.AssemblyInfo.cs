197e04d316bffe6b3efc8782ed012076c0b9fd46
LucasBXavier <lucas_b.xavier@hotmail.com>
2024-03-26 22:38:32 -0300

    atualização em model e DB para mais ingredientes, mudança no front para incluir doces

--- back/src/PanelaMagica.API/obj/Debug/net6.0/PanelaMagica.API.AssemblyInfo.cs
+++ back/src/PanelaMagica.API/obj/Debug/net6.0/PanelaMagica.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PanelaMagica.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b64fc96ba80744a61301cbe73479217b618692ae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e639c8726d85030a3544a4943015715483bac3b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("PanelaMagica.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PanelaMagica.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
