227861a2e0574d32369eb51777f404dc01dcb643
LucasBXavier <lucas_b.xavier@hotmail.com>
2024-03-28 14:25:49 -0300

    alterações de responsividade

--- back/src/PanelaMagica.API/obj/Debug/net6.0/PanelaMagica.API.AssemblyInfo.cs
+++ back/src/PanelaMagica.API/obj/Debug/net6.0/PanelaMagica.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PanelaMagica.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+197e04d316bffe6b3efc8782ed012076c0b9fd46")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da8f6a63dc92b6d6a4b2fb77d462ba971a8c105e")]
 [assembly: System.Reflection.AssemblyProductAttribute("PanelaMagica.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PanelaMagica.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
