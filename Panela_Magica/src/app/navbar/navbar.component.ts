62189bab20a47a6cf5f65dcca36f4cbd257e9b5b
RhoanBarioni <rhoanbarioni@gmail.com>
2024-05-28 16:24:09 -0300

    Pack de correções (OFFCANVAS)

--- Panela_Magica/src/app/navbar/navbar.component.ts
+++ Panela_Magica/src/app/navbar/navbar.component.ts
@@ -1,30 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-}
-
-const menuBtn = document.querySelector('.offcanvas-btn') //as HTMLElement | null;
-const offcanvas = document.querySelector('.offcanvas') //as HTMLElement | null;
-
-function toggleOffcanvas() {
+export class NavbarComponent {
+  toggleOffcanvas(): void {
+    const menuBtn = document.querySelector('#offcanvas-btn');
+    const offcanvas = document.querySelector('#offcanvas');
+    
     if (menuBtn && offcanvas) {
-        menuBtn.classList.toggle('change');
-        offcanvas.classList.toggle('active');
+      menuBtn.classList.toggle('change');
+      offcanvas.classList.toggle('active');
     }
+  }
 }
-
-if (menuBtn) {
-    menuBtn.addEventListener('click', toggleOffcanvas);
-}
-
